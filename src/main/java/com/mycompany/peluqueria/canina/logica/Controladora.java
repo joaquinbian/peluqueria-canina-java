66755ab3201989d2d5090806276d84527988bb27
Joaquin Bianchi <joacobian21@gmail.com>
2024-02-09 01:06:28 -0300

    instanciamos las clases JPAController y la controladora de persistencia

--- src/main/java/com/mycompany/peluqueria/canina/logica/Controladora.java
+++ src/main/java/com/mycompany/peluqueria/canina/logica/Controladora.java
@@ -1,6 +1,11 @@
 
 package com.mycompany.peluqueria.canina.logica;
 
+import com.mycompany.peluqueria.canina.persistencia.ControladoraPersistencia;
+
 public class Controladora {
+    ControladoraPersistencia controladoraPersistencia = new ControladoraPersistencia();
+    
+    
     
 }
--- src/main/java/com/mycompany/peluqueria/canina/persistencia/ControladoraPersistencia.java
+++ src/main/java/com/mycompany/peluqueria/canina/persistencia/ControladoraPersistencia.java
@@ -1,8 +1,8 @@
 
 package com.mycompany.peluqueria.canina.persistencia;
 
-import com.mycompany.peluqueria.canina.logica.Mascota;
 
 public class ControladoraPersistencia {
-    Mascota mascota = new Mascota();
+     MascotaJpaController mascotaJpaController = new MascotaJpaController();
+     DuenoJpaController duenoJpaController = new DuenoJpaController();
 }

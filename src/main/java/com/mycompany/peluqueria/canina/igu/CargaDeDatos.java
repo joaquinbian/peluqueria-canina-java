7c49c1e236ce439faf1ca161b67a609dad2ee5da
Joaquin Bianchi <joacobian21@gmail.com>
2024-02-08 23:00:02 -0300

    funcionalidad de funcion limpiar

--- src/main/java/com/mycompany/peluqueria/canina/igu/CargaDeDatos.java
+++ src/main/java/com/mycompany/peluqueria/canina/igu/CargaDeDatos.java
@@ -119,6 +119,11 @@ public class CargaDeDatos extends javax.swing.JFrame {
         });
 
         cleanBtn.setText("Limpiar");
+        cleanBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cleanBtnActionPerformed(evt);
+            }
+        });
 
         saveBtn.setText("Guardar");
 
@@ -268,40 +273,23 @@ public class CargaDeDatos extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_hasSpecialAtentionActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(CargaDeDatos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(CargaDeDatos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(CargaDeDatos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(CargaDeDatos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
+    private void cleanBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cleanBtnActionPerformed
+        // TODO add your handling code here:
+        celDuenioTextField.setText("");
+        colorTextField.setText("");
+        nameDuenioTextField.setText("");
+        nameTextField.setText("");
+        obervationsTextArea.setText("");
+        razaTextField.setText("");
+        
+        
+        isAlergico.setSelectedIndex(0);
+        hasSpecialAtention.setSelectedIndex(0);
+        
+        
+    }//GEN-LAST:event_cleanBtnActionPerformed
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new CargaDeDatos().setVisible(true);
-            }
-        });
-    }
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTextField celDuenioTextField;
